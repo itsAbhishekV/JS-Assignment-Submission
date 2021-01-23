a2673805e8f48eb55d5c76e998cd7c804339627d
Abhishek Verma <70279771+itsAbhishekV@users.noreply.github.com>
2021-01-23 13:27:23 +0530

    Assignment - 2

---
+++ Day 3 -Assignment 2/Question-2.js
@@ -0,0 +1,14 @@
+/*Lets Upgrade
+	Day-3
+		Assignment - 2
+			Question - 2*/
+
+
+var arr = [/*Some elements in here*/];
+
+var sum = 0 //assigning initial value for a varient 
+
+for(var i in arr) {
+	sum+=arr[i]
+ }
+console.log(sum)
